8f085e7609e5f41442176e2ad2d5a64821c555d7
Ermerson Rafael <ermersonrafael@hotmail.com>
2026-06-06 16:42:45 -0300

    test(aspnetcore): add end-to-end test for custom AuthEnricherOptions through AddWideEvents

--- tests/WideEvents.AspNetCore.Tests/WideEventExtensionsTests.cs
+++ tests/WideEvents.AspNetCore.Tests/WideEventExtensionsTests.cs
@@ -1,5 +1,8 @@
+using System.Security.Claims;
 using FluentAssertions;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using WideEvents.Core.Context;
 using Xunit;
 
 namespace WideEvents.AspNetCore.Tests;
@@ -43,5 +46,28 @@ public sealed class WideEventExtensionsTests
         var provider = services.BuildServiceProvider();
         var enrichers = provider.GetServices<IHttpWideEventEnricher>();
         enrichers.Should().Contain(e => e is DefaultHttpEnricher);
+    }
+
+    [Fact]
+    public void AddWideEvents_WithCustomAuthEnricherOptions_HonorsConfiguration()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddWideEvents(o => o.UseAuthEnricher(a =>
+        {
+            a.ClaimType = "sub";
+            a.FieldName = "auth.uid";
+        }));
+        var provider = services.BuildServiceProvider();
+        var enricher = provider.GetServices<IHttpWideEventEnricher>().OfType<AuthEnricher>().Single();
+
+        var ctx = new DefaultHttpContext();
+        ctx.User = new ClaimsPrincipal(new ClaimsIdentity([new Claim("sub", "user_789")]));
+        var wideEvent = new WideEventContext(enrichers: []);
+        enricher.EnrichRequest(ctx, wideEvent);
+
+        var built = wideEvent.Build();
+        var auth = built["auth"].Should().BeAssignableTo<IReadOnlyDictionary<string, object?>>().Subject;
+        auth["uid"].Should().Be("user_789");
     }
 }
